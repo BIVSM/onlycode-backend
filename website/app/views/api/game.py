a693bc199f635dbe99d3763b5c2dc104f03a1461
DerwesX <derwesx@yandex.ru>
2024-05-19 18:40:37 +0300

    game API added

--- website/app/views/api/game.py
+++ website/app/views/api/game.py
@@ -3,7 +3,9 @@ from app.models import Game
 
 from django.core import serializers
 from django.http import HttpRequest
+from django.db import transaction
 import ast
+
 
 class GameView(APIView):
     @takes_json
@@ -27,8 +29,32 @@ class GameView(APIView):
                 serialized_game['rules'] = 'Rules does not exist'
             return self.render_json(200, {'game': serialized_game})
 
-
-class GameCreateView(APIView):
     @takes_json
     def post(self, request: HttpRequest):
-        ...  # TODO
+        game_id = str(self.json_input.get("id"))
+        with transaction.atomic():
+            game_model = Game.objects.get(pk=game_id)
+            if self.json_input.get("name"):
+                game_model.name = str(self.json_input.get("name"))
+            if self.json_input.get("number_of_players"):
+                game_model.number_of_players = str(self.json_input.get("number_of_players"))
+            if self.json_input.get("win_point"):
+                game_model.win_point = str(self.json_input.get("win_point"))
+            if self.json_input.get("lose_point"):
+                game_model.lose_point = str(self.json_input.get("lose_point"))
+            if request.FILES.get('ideal_solution') is not None:
+                game_model.ideal_solution = request.FILES.get('ideal_solution')
+            if request.FILES.get('play') is not None:
+                game_model.play = request.FILES.get('play')
+            if request.FILES.get('visualiser') is not None:
+                game_model.visualiser = request.FILES.get('visualiser')
+            if request.FILES.get('rules') is not None:
+                game_model.rules = request.FILES.get('rules')
+            game_model.save()
+        return self.render_json(204, {'result': 'Game ' + game_id + ' changed'})
+
+
+class GameCreateView(APIView):
+    def post(self, request: HttpRequest):
+        game_model = Game.objects.create()
+        return self.render_json(200, {'result': 'Game was successfully created', 'id': game_model.id})
