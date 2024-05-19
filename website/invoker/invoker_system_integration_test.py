3f2b7f9f9da99f11396e34de1acd0bd216c8b8c7
DerwesX <derwesx@yandex.ru>
2024-05-19 14:45:47 +0300

    IntTest Fix

--- website/invoker/invoker_system_integration_test.py
+++ website/invoker/invoker_system_integration_test.py
@@ -3,14 +3,17 @@ import os
 
 from django.test import TestCase
 from django.conf import settings
+from django.db import connections
 
 from app.compiler import Compiler, CompilerReport
 from app.launcher import Launcher
 from .invoker import Invoker, NormalProcess
+from .invoker_pool import InvokerPool
 
 
 class TestInvokerSystem(TestCase):
     def test(self):
+        invoker_pool = InvokerPool()
         for lang in settings.SUPPORTED_LANGUAGES:
             file = os.path.abspath('invoker/invoker_system_integration_test/invoker_integration.{}'.format(lang))
 
@@ -24,16 +27,16 @@ class TestInvokerSystem(TestCase):
 
             def get_compiler_callback(report: CompilerReport):
                 self.compiler_call = True
-                invoker = Invoker()
+                invoker = invoker_pool.get(1)[0]
                 compiled_file = report.compiled_file.path
                 launcher = Launcher(compiled_file, process_callback=get_launcher_callback)
                 launcher.run(invoker)
 
             compiler = Compiler(file, lang, get_compiler_callback)
             compiler.compile()
-
             counter = time.perf_counter()
-            while ((time.perf_counter() - counter) < 14) & ((self.launch_call & self.compiler_call) == False):
+            while ((time.perf_counter() - counter) < settings.COMPILE_TL[lang]) and (
+                    self.launch_call & self.compiler_call) is False:
                 pass
             self.assertEqual(self.compiler_call, True, "language: {} is not compiling".format(lang))
             self.assertEqual(self.launch_call, True, "language: {} is not launching".format(lang))
